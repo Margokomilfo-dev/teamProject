b953848e24628bf013842ca4d6c8b6833432833e
alekseibaranov <baranov.sys@gmail.com>
2021-03-11 23:46:28 +0300

    PassRecovery - ok devBaranov

--- src/Component/Pages/PasswordRecovery/PasswordRecovery.tsx
+++ src/Component/Pages/PasswordRecovery/PasswordRecovery.tsx
@@ -1,18 +1,61 @@
 import React from 'react'
-import s from './PasswordRecovery.module.css'
+import {useFormik} from 'formik'
+import {Button, FormControl, FormGroup, Grid, TextField} from "@material-ui/core";
+import {passRecTC} from "../../../redux/forgotReducer";
+import {useDispatch} from "react-redux";
+
+type PassRecType = {
+	email: string
+	from: string
+	message: string
+}
 
 export const PasswordRecovery = () => {
+
+	type FormikErrosType = {
+		email?: string
+	}
+	const dispatch = useDispatch()
+
+	const formik = useFormik({
+		initialValues: {
+			email: '',
+			from: 'test-front-admin <baranov.sys@gmail.com>',
+			message: `<div style="background-color: lime;padding: 15px> password recovery link: 
+<a href='http://localhost:3000/teamProject#/new-pass/$token$'>link </a> </div>`
+		},
+		validate: (values: PassRecType) => {
+			const errors: FormikErrosType = {};
+			if (!values.email) {
+				errors.email = 'Обезательно для ввода';
+			} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+				errors.email = 'Не корректный email';
+			}
+			return errors
+		},
+		onSubmit: values => {
+			dispatch(passRecTC ({email: values.email, from: values.from, message: values.message} ))
+			formik.resetForm()
+		}
+	})
+
 	return (
-		<div className={s.passRecOverlay}>
-			PasswordRecovery
-			devBaranov
-			devBaranov
-			devBaranov
-			devBaranov
-			devBaranov
-			devBaranov
-			devBaranov
-			devBaranov
-		</div>
+		<Grid container justify="center" alignItems="stretch" style={{height: '100vh'}}>
+			<Grid container justify="center" alignItems="center" >
+				<form onSubmit={formik.handleSubmit}>
+					<FormControl>
+						<FormGroup>
+							<TextField
+								label='Email'
+								margin = 'normal'
+								{...formik.getFieldProps('email')}
+							/>
+							{ formik.touched.email && formik.errors.email ? <div style={{color: "red"}}>{formik.errors.email}</div> : null}
+							<Button type={'submit'} variant={'contained'} color={'primary'}>Send email</Button>
+						</FormGroup>
+					</FormControl>
+				</form>
+			</Grid>
+		</Grid>
 	)
 }
---
+++ src/redux/forgotReducer.ts
@@ -0,0 +1,34 @@
+import API, {PassRecType} from '../api/api'
+import {Dispatch} from 'redux'
+
+enum ACTION_TYPE {
+	SET_OK = 'forgotReducer/SET_OK'
+}
+
+const initialState = {
+	answer: false,
+	html: false,
+	info: '',
+	success: true
+}
+
+type InitialStateType = typeof initialState
+
+export const forgotReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
+	switch (action.type) {
+		case ACTION_TYPE.SET_OK:
+			return {...state, info: action.info, answer: true, html: true}
+		default:
+			return state
+	}
+}
+
+export const setOkPassRecAC = (info: string) => ({type: ACTION_TYPE.SET_OK, info} as const)
+
+export const passRecTC = (data: PassRecType) => (dispatch: Dispatch<ActionType>) => {
+	API.passRec(data.email, data.from, data.message).then(res => {
+		dispatch(setOkPassRecAC(res.info))
+	})
+}
+
+type ActionType = ReturnType<typeof setOkPassRecAC>
