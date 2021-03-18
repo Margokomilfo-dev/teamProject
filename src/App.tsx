f58aabb011a2dd644a1c7158f1de2b57dad4c0e9
alekseibaranov <baranov.sys@gmail.com>
2021-03-18 14:32:51 +0300

    fix HashRouter in App.tsx for link email

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import './App.css'
-import {HashRouter, Redirect} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import Routes from './Component/Routes/Routes'
 import Header from './Component/Header/Header'
-import {useDispatch, useSelector} from 'react-redux'
-import {AppRootStateType} from './redux/store'
+
 
 function App() {
-    // const isInitialized = useSelector<AppRootStateType>(state => state.app.isInitialized)
-    // if (!isInitialized) {
-    //     return <Redirect to={'login'}/>
-    // }
-
     return (
         <>
+          <HashRouter>
             <Header/>
             <Routes/>
+          </HashRouter>
         </>
     )
 }
